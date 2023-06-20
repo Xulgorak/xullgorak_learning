ab7ee7f6c5384f693398c2debecdb3d94385ac78
xulgorak <xulgorak@gmail.com>
2023-06-20 13:28:36 +0300

    first practice1

--- main.js
+++ main.js
@@ -24,3 +24,10 @@
 'use strict';
 
 const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+	'count': numberOfFilms,
+	'movies': {},
+	'actors': {},
+	'genres': [],
+	'privat': false
+};
