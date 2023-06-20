d9c212fa67c8a472791dcd81aa7f77562e0f662e
xulgorak <xulgorak@gmail.com>
2023-06-20 16:12:11 +0300

    second lesson(1)

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 'use strict';
 
+let num = 50;
+
+for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+        //break;
+        continue;
+    }
+	console.log(i);
+}
+
+for (let i = 0, i < 3, i++) {
+    console.log(i);
+    for (let j = 0, j < 3, j++) {
+        console.log(j);
+    }
+}
+
+
+
